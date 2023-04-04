32ac05dbcab3c652f5279dc75a8690ed64718925
Ben Lawrence <12blawrence4@gmail.com>
2023-04-04 21:34:02 +1200

    Added a footer
    Added a basic footer for contact details etc.
    Needs improvement

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <router-view/>
+  <footer>
+    Hello all
+  </footer>
 </template>
 
 <script>
@@ -29,6 +32,18 @@ main {
   width: 100%;
   height: 100%;
   font-family: Roboto;
+  overflow: hidden;
+}
+
+footer {
+  position: fixed;
+  bottom: 0;
+
+  height: 50px;
+  width: 100vw;
+  width: 100dvw; //Override with device view width
+
+  background-color: #F9C823;
 }
 
 </style>
